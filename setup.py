3e73ff98669e6cd96ff3978e1611318f4accbba3
Fabian Peter Hammerle <fabian.hammerle@gmail.com>
2016-10-16 17:25:26 +0200

    code initial

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+import glob
+
+setup(
+    name = 'duplitab',
+    version = '0.1',
+    description = '',
+    author = 'Fabian Peter Hammerle',
+    author_email = 'fabian.hammerle@gmail.com',
+    url = 'https://github.com/fphammerle/duplitab',
+    download_url = 'https://github.com/fphammerle/duplitab/tarball/0.1',
+    keywords = ['backup', 'duplicity'],
+    classifiers = [],
+    scripts = glob.glob('scripts/*'),
+    install_requires = ['argparse'],
+    tests_require = ['pytest']
+    )

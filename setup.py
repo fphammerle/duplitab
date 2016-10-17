09eef71fcab387bf1ab7ab2886f6e6d99d0b29fa
Fabian Peter Hammerle <fabian.hammerle@gmail.com>
2016-10-17 09:58:13 +0200

    added missing dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 setup(
     name = 'duplitab',
     version = '0.1',
-    description = '',
+    description = 'wrapper for duplicity featuring persistent backup configuration',
     author = 'Fabian Peter Hammerle',
     author_email = 'fabian.hammerle@gmail.com',
     url = 'https://github.com/fphammerle/duplitab',
@@ -13,6 +13,10 @@ setup(
     keywords = ['backup', 'duplicity'],
     classifiers = [],
     scripts = glob.glob('scripts/*'),
-    install_requires = ['argparse', 'pyyaml'],
+    install_requires = [
+        'argparse',
+        'pyyaml',
+        'tabulate',
+        ],
     tests_require = ['pytest']
     )
